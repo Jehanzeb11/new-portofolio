8cd4b6dd155fe800a1c3258dbf9aba75722a30cd
jehanzeb <jehanzebsiddiqui287@gmail.com>
2024-06-28 19:00:19 +0500

    skills remaining

---
+++ src/components/Skills.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Skills = () => {
+  return (
+    <div className='flex justify-center items-center flex-col max-md:mt-16'>
+        <h4 className='uppercase text-gray-400 md:text-3xl text-2xl font-medium text-center'>Skills</h4>
+    <h1 className='text-gray-200 md:text-6xl text-4xl font-semibold mt-4 text-center'>Technical <span className='text-primary'>Skills</span></h1>
+
+    </div>
+  )
+}
+
+export default Skills
